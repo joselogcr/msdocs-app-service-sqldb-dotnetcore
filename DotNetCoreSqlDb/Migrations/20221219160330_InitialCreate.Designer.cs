1b61b1e66793aae7a874060cc52826577897fe29
joselogcr <130867319+joselogcr@users.noreply.github.com>
2023-04-15 18:44:46 -0600

    Todo v2

--- DotNetCoreSqlDb/Migrations/20221219160330_InitialCreate.Designer.cs
+++ DotNetCoreSqlDb/Migrations/20221219160330_InitialCreate.Designer.cs
@@ -19,7 +19,7 @@ namespace DotNetCoreSqlDb.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "6.0.0")
+                .HasAnnotation("ProductVersion", "7.0.0")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder, 1L, 1);
